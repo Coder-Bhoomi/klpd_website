9d35e7f87164baf35c48ee7dd75e8769200d6e69
Coder-Bhoomi <144001574+Coder-Bhoomi@users.noreply.github.com>
2024-10-29 21:01:40 +0530

    first commit

---
+++ klpd/src/main/java/com/klpdapp/klpd/KlpdApplication.java
@@ -0,0 +1,13 @@
+package com.klpdapp.klpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlpdApplication.class, args);
+	}
+
+}
