8f7fade072dcc7d6d585181bc16d6b852fde0b9d
Tanya <tanyashukla09102003@gmail.com>
2024-11-06 18:47:37 +0530

    Merge branch 'main' of https://github.com/Coder-Bhoomi/klpd_website

---
+++ klpd/src/main/java/com/klpdapp/klpd/dto/sizedto.java
@@ -0,0 +1,19 @@
+package com.klpdapp.klpd.dto;
+
+public class sizedto {
+    private String categoryId;
+    private String size;
+    public String getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+}
---
+++ klpd/src/main/java/com/klpdapp/klpd/model/size.java
@@ -0,0 +1,26 @@
+package com.klpdapp.klpd.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "size")
+public class size {
+    @Column (length = 15)
+    private String categoryId;
+    @Column (length = 50)
+    private String size;
+    public String getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
