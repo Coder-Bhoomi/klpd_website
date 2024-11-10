855160ae0bdcbf01ea3a82f617022473f5a8704c
Coder-Bhoomi <144001574+Coder-Bhoomi@users.noreply.github.com>
2024-11-10 22:52:02 +0530

    Basic changes

--- klpd/src/main/resources/static/js/script.js
+++ klpd/src/main/resources/static/js/script.js
@@ -1,3 +1,9 @@
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
+
+// Remove the loading screen once the page is fully loaded
+window.addEventListener("load", function() {
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+});
